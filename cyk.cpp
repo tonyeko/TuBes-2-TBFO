091bb7e6b5262c94d229591f9ff8e842e89f0b5c
V <48679462+haverzard@users.noreply.github.com>
2019-11-17 16:01:28 +0700

    Added Input File + grammar

--- cyk.cpp
+++ cyk.cpp
@@ -1,21 +1,32 @@
 #include <fstream>
 #include <iostream>
- 
+#include <bits/stdc++.h>
+
 #include "grammar.hpp"
 #include "cyk.hpp"
  
+using namespace std;
 int main()
 {
-    std::string filename = "cyk.dat";
-    std::ifstream ifs(filename);
-    if (ifs) {
+    string filename = "cyk.dat";
+    string inputfile;
+    cin >> inputfile;
+    ifstream ifs(filename);
+    ifstream ifs2(inputfile);
+    if (ifs && ifs2) {
         MB::grammar grammar(ifs);
-        std::string sentence[] = {"a", "1", "' '", "=", "1", "0", "0"};
+        string bacafile;
+        getline(ifs2, bacafile, '\0');
+        string sentence[bacafile.length()];
+        for (int i = 0; i < bacafile.length(); i++) {
+            sentence[i] = bacafile[i];
+        }
+        //cout << bacafile << endl;
         const size_t len = sizeof(sentence) / sizeof(sentence[0]);
-        bool success = MB::cyk_parser(grammar).parse(sentence, sentence + len, std::cout);
-        std::cout << "Success: " << std::boolalpha << success << '\n';
+        bool success = MB::cyk_parser(grammar).parse(sentence, sentence + len, cout);
+        cout << "Success: " << boolalpha << success << '\n';
     }
     else {
-        std::cerr << "Couldn't open " << filename << " for reading\n";
+        cerr << "Couldn't open " << filename << " for reading\n";
     }
 }
--- generate_grammar1.py
+++ generate_grammar1.py
@@ -8,4 +8,8 @@ with open("grammar1.dat", "w") as f:
 	for i in range(len(all_chars)):
 		f.write('Chr'+all_chars[i]+' -> '+all_chars[i]+'\n')
 		X += all_chars[i] + " | "
+	f.write('ChrVar -> '+X[:-2]+'\n')
+	X = ""
+	for i in range(len(string.printable[:-6])):
+		X += string.printable[i] + " | "
 	f.write('Chr -> '+X[:-2]+'\n')
